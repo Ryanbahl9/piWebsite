aa99fbc00580af992a558987237f8016b3e4fd0a
Ryanbahl9 <Ryanbahl9@gmail.com>
2019-12-03 13:43:33 -0800

    Update inetSpeedCollecter.py

--- inetSpeedCollecter.py
+++ inetSpeedCollecter.py
@@ -2,13 +2,12 @@ import datetime
 import mysql.connector
 import speedtest
 
-
 curr_min = datetime.datetime.now().strftime("%M")
 
-if (int(curr_min) % 15 == 0):
+if (int(curr_min) % 3 == 0):
 #if (0==0):
     cnx = mysql.connector.connect(user='dev0', password='Christa1',
-                                  host='127.0.0.1',
+                                  host='73.158.191.112',
                                   database='inetSpeed')
     cursor = cnx.cursor()
     servers = []
@@ -17,23 +16,10 @@ if (int(curr_min) % 15 == 0):
     st = speedtest.Speedtest()
     st.get_servers(servers)
     st.get_best_server()
-    
-    downLst = list()
-    upLst = list()
-    
-    startTime = datetime.datetime.now()
-    for x in range(0, 3):
-        downLst.append( st.download(threads=threads) / 1000000)
-        upLst.append( st.upload(threads=threads) / 1000000)
-        datetime.time.sleep(1)
-    endTime = datetime.datetime.now()
 
-    down = (downLst[0] + downLst[1] + downLst[2]) / 3
-    up = (upLst[0] + upLst[1] + upLst[2]) / 3
-    
-    timeDiff = (endTime - startTime) / 2
-    aveTime = startTime + timeDiff
-    dt = aveTime.strftime('%Y-%m-%d %H:%M:%S')
+    dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    down = (st.download(threads=threads) / 1000000)
+    up = (st.upload(threads=threads) / 1000000)
 
     cursor.execute("INSERT INTO inetLog(datetime, speed_down, speed_up) "
                    "VALUES (%s, %s, %s)", (dt, down, up))
@@ -41,4 +27,3 @@ if (int(curr_min) % 15 == 0):
 
     cursor.close()
     cnx.close()
-
